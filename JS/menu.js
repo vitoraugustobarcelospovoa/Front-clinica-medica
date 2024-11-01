fcc8e276df4902491647f2dce0a9ba0622287e55
MarcoTulio2023 <147112543+MarcoTulio2023@users.noreply.github.com>
2024-11-01 09:35:45 -0300

    Tela Inicial Final

--- JS/menu.js
+++ JS/menu.js
@@ -2,3 +2,12 @@ function toggleMenu() {
     const menu = document.getElementById("navMenu");
     menu.classList.toggle("active");
 }
+
+const navLinks = document.querySelectorAll('.nav-menu a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        const menu = document.getElementById("navMenu");
+        menu.classList.remove('active'); // Remove a classe 'active' para fechar o menu
+    });
+});
